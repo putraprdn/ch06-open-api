d9510151573f86d928006d413d14b4f52cbed2af
putraperdana138 <putraperdana138@students.unnes.ac.id>
2022-05-12 11:27:00 +0700

    Rewrite route

--- config/routes.js
+++ config/routes.js
@@ -3,6 +3,8 @@ const controllers = require("../app/controllers");
 // const { registerRules } = require("../app/validators/rule");
 // const checkToken = require("../app/middlewares/checkToken");
 // const validate = require("../app/middlewares/validate");
+
+// Some dependencies for api documenations
 const YAML = require("yamljs");
 const swaggerUI = require("swagger-ui-express");
 const apiDocs = YAML.load("./api-doc.yaml");
@@ -14,42 +16,18 @@ const apiRouter = express.Router();
  *       implementations
  */
 
-// open api endpoint using swagger ui
+// api documentation endpoint using swagger ui
 apiRouter.use("/api-docs/v1", swaggerUI.serve, swaggerUI.setup(apiDocs));
 
 apiRouter.get("/api/v1/posts", controllers.api.v1.postController.list);
 apiRouter.post("/api/v1/posts", controllers.api.v1.postController.create);
 apiRouter.put("/api/v1/posts/:id", controllers.api.v1.postController.update);
 apiRouter.get("/api/v1/posts/:id", controllers.api.v1.postController.show);
-apiRouter.delete(
-	"/api/v1/posts/:id",
-	controllers.api.v1.postController.destroy
-);
+apiRouter.delete("/api/v1/posts/:id",controllers.api.v1.postController.destroy);
 
-apiRouter.get(
-	"/api/v1/whoami",
-	controllers.api.v1.userController.checkToken,
-	controllers.api.v1.userController.whoAmI
-);
+// User endpoint
 apiRouter.post("/api/v1/login", controllers.api.v1.userController.login);
-apiRouter.post(
-	"/api/v1/register",
-	controllers.api.v1.validatorController.validate(
-		controllers.api.v1.validatorController.registerRules
-	),
-	controllers.api.v1.userController.register
-);
-/**
- * TODO: Delete this, this is just a demonstration of
- *       error handler
- */
-// apiRouter.get("/api/v1/errors", () => {
-// 	throw new Error(
-// 		"The Industrial Revolution and its consequences have been a disaster for the human race."
-// 	);
-// });
-
-// apiRouter.use(controllers.api.main.onLost);
-// apiRouter.use(controllers.api.main.onError);
+apiRouter.post("/api/v1/register",controllers.api.v1.validatorController.validate(controllers.api.v1.validatorController.registerRules),controllers.api.v1.userController.register);
+apiRouter.get("/api/v1/whoami",controllers.api.v1.userController.checkToken,controllers.api.v1.userController.whoAmI);
 
 module.exports = apiRouter;
